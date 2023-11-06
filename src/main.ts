9321407bd2d6124a89b1f25684e4b3e10c942073
jeff.cofos <jeff.cofos@gmail.com>
2023-11-06 21:37:54 +0900

    init

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import helmet from "helmet";
 import router from "./router";
 import { config } from "dotenv";
 import { PrismaClient } from "@prisma/client";
+import favicon from "express-favicon";
+
 config();
 
 const app = express();
 
 app.use(helmet());
+app.use(favicon(__dirname + '/public/favicon.png'));
 
 const whitelist = ["http://localhost:3000", "http://localhost:8000","http://127.0.0.1:3000", "http://127.0.0.1:8000"];
 
@@ -21,7 +24,10 @@ const corsOptions : cors.CorsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use('/', router);
+app.use("/", (req, res) => {
+  res.status(200).send({ message: "server is running ok" })
+});
+app.use('/api', router);
 
 app.use(function (req, res, next) {
   res.setHeader("Content-Type", "application/json");

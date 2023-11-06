a63aba934a00c20067f5fff8bf96ef24b1d12002
jeff.cofos <jeff.cofos@gmail.com>
2023-11-06 21:56:48 +0900

    init

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(helmet());
 app.use(favicon(path.resolve(__dirname, "../public", "favicon.ico")));
 
-const whitelist = ["http://localhost:3000", "http://localhost:8000","http://127.0.0.1:3000", "http://127.0.0.1:8000"];
+const whitelist = ["*"];
 
 const corsOptions : cors.CorsOptions = {
   origin: whitelist,
